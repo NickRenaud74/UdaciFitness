036c7125139601ed50a0fed5ccb4593fff5aded6
NickRenaud74 <nick_renaud@hotmail.com>
2021-02-18 10:25:42 -0800

    add redux store wrapper to app

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 import AddEntry from './components/AddEntry'
 
 export default function App() {
-    return ( 
-    <View>
-        <AddEntry />
-    </View>
-    );
+    return (
+        <Provider store={createStore(reducer)}>
+            <View>
+                <AddEntry />
+            </View>
+        </Provider>
+    )
 }
