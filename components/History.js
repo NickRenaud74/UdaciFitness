d20ae9e8a543887d42488f31a1732ab7e21f8290
NickRenaud74 <nick_renaud@hotmail.com>
2021-02-21 16:31:06 -0800

    add styles to unlogged data days

--- components/History.js
+++ components/History.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
+import { white } from '../utils/colors'
 import { Calendar, Agenda } from 'react-native-calendars'
 
 function History() {
@@ -24,18 +25,24 @@ function History() {
 
     function renderItem({ today, ...metrics }, formattedDate, key) {
         return (
-            <View>
+            <View style={styles.item}> 
                 { today
-                    ? <Text>{JSON.stringify(today)}</Text>
-                    : <Text>{JSON.stringify(metrics)}</Text>}
+                    ? <View>
+                        <Text style={styles.noDataText}>{today}</Text>
+                    </View>
+
+                    : <TouchableOpacity onPress={() => console.log('pressed')} >
+                        <Text>{JSON.stringify(metrics)}</Text>
+                    </TouchableOpacity>
+                    }
             </View>
         )
     }
 
     function renderEmptyDate(formattedDate) {
         return (
-            <View>
-                <Text>No Data for this day</Text>
+            <View style={styles.item}>
+                <Text style={styles.noDataText}>You didn't log any data on this day</Text>
             </View>
         )
     }
@@ -50,5 +57,29 @@ function History() {
         />
     )
 }
+
+const styles = StyleSheet.create({
+    item: {
+      backgroundColor: white,
+      borderRadius: Platform.OS === 'ios' ? 16 : 2,
+      padding: 20,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 17,
+      justifyContent: 'center',
+      shadowRadius: 3,
+      shadowOpacity: 0.8,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+    },
+    noDataText: {
+      fontSize: 20,
+      paddingTop: 20,
+      paddingBottom: 20
+    }
+  })
 
 export default History
